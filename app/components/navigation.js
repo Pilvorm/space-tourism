8c7220465d7dde8b7791878837e406ec8714d9f0
Daniel Emerald Sumarly <danielemerald@ymail.com>
2025-05-17 18:25:52 +0700

    responsive homepage

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,6 +1,11 @@
+"use client";
+
 import Logo from "@/public/assets/shared/logo.svg";
+import Menu from "@/public/assets/shared/icon-hamburger.svg";
 import { barlow, barlowCondensed } from "../fonts";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -8,43 +13,66 @@ const navLinks = [
     link: "Home",
   },
   {
-    href: "/",
+    href: "/destination",
     link: "Destination",
   },
   {
-    href: "/",
+    href: "/crew",
     link: "Crew",
   },
   {
-    href: "/",
+    href: "/technology",
     link: "Technology",
   },
 ];
 
 const Navigation = ({}) => {
-  return (
-    <nav className="pl-16 pt-10 flex items-center justify-between">
-  <div className="flex items-center gap-16 relative">
-    <Image src={Logo} width={48} height={48} alt="Space Tourism Logo" />
-    <div className="horizontal-line z-[99] absolute left-[112px] bottom-[50%] w-[1500%] h-[1.5px]"></div>
-  </div>
-  <ul className="min-w-1/2 h-24 flex items-center justify-end gap-12 px-16 backdrop-blur-[80px] bg-white/5 relative">
-    {navLinks.map((nav, idx) => (
-      <li
-        key={nav.link}
-        className="cursor-pointer h-full flex items-center gap-3 text-white border-b-2 border-transparent transition-all hover:border-white"
-      >
-        <span className="barlow-condensed font-bold tracking-[2.7px]">
-          {idx.toString().padStart(2, "0")}
-        </span>
-        <p className="barlow-condensed font-light uppercase tracking-[2px]">
-          {nav.link}
-        </p>
-      </li>
-    ))}
-  </ul>
-</nav>
+  const pathname = usePathname();
+  console.log(pathname);
 
+  return (
+    <nav className="py-6 flex items-center justify-between">
+      <div className="flex items-center px-6 md:px-10 gap-16 relative">
+        <Image
+          src={Logo}
+          className="w-10 h-10 md:w-12 md:h-12"
+          alt="Space Tourism Logo"
+        />
+        <div className="horizontal-line hidden lg:block z-[99] absolute left-[112px] bottom-[50%] w-[15vw] xl:w-[40vw] 2xl:w-[45vw] h-[1.5px]"></div>
+      </div>
+
+      <button className="px-6 md:hidden">
+        <Image src={Menu} width={24} height={21} alt="" />
+      </button>
+
+      <ul className="hidden md:flex items-center justify-end gap-12 px-6 md:px-10 lg:px-16 w-auto lg:min-w-1/2 h-24 backdrop-blur-[80px] bg-white/5 relative">
+        {navLinks.map((nav, idx) => {
+          const isActive = pathname === nav.href;
+          return (
+            <li
+              key={nav.link}
+              className={`cursor-pointer h-full text-white border-b-2 transition-all ${
+                isActive
+                  ? "border-white"
+                  : "border-transparent hover:border-white"
+              }`}
+            >
+              <Link
+                href={nav.href}
+                className="h-full flex items-center gap-3 text-white"
+              >
+                <span className="barlow-condensed font-bold tracking-[2.7px]">
+                  {idx.toString().padStart(2, "0")}
+                </span>
+                <p className="barlow-condensed font-light uppercase tracking-[2px]">
+                  {nav.link}
+                </p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -1,27 +1,38 @@
 import Link from "next/link";
-import { motion, useAnimation } from 'framer-motion';
+import { motion, useAnimation } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-between px-32 pt-64 pb-32">
-      <div>
-        <p className="uppercase barlow-condensed text-[28px] tracking-[4px]">
-          So, You Want to Travel To
-        </p>
-        <h1 className="bellefair text-white text-[144px]">SPACE</h1>
-        <p className="max-w-xl barlow font-light text-lg">
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+    <main
+      className="md:min-h-screen flex flex-col lg:justify-end py-6 md:py-24 px-6 md:px-10 lg:px-32"
+    >
+      <div className="flex flex-col lg:flex-row items-center lg:items-end justify-between gap-12 px-0 md:px-12 text-center lg:text-left">
+        {/* TEXTS */}
+        <div className="flex flex-col gap-6">
+          <p className="uppercase barlow-condensed text-base md:text-[28px] tracking-[4px]">
+            So, You Want to Travel To
+          </p>
+          <h1 className="bellefair text-white text-[80px] md:text-[144px] leading-none">
+            SPACE
+          </h1>
+          <p className="max-w-xl barlow font-light text-base leading-[180%]">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+
+        {/* BUTTON */}
+        <div className="py-16 flex items-center justify-center">
+          <Link
+            href="/"
+            className="w-[144px] h-[144px] md:w-[272px] md:h-[272px] rounded-full bg-white flex items-center justify-center text-black"
+          >
+            <h2 className="bellefair text-lg md:text-[32px]">EXPLORE</h2>
+          </Link>
+        </div>
       </div>
-      <Link
-        href="/"
-        className="relative w-72 h-72 rounded-full bg-white flex items-center justify-center text-black"
-      >
-        <h2 className="bellefair text-[32px]">EXPLORE</h2>
-      </Link>
     </main>
   );
 }
